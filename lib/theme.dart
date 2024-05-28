ff4c3ce3f9d9c8e066253280aa3f4e8a6df26ecb
coleritchiee <critchi1@binghamton.edu>
2024-05-27 20:33:21 -0400

    Theme Setup

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+var appTheme = ThemeData(
+  fontFamily: GoogleFonts.notoSans().fontFamily,
+  brightness: Brightness.dark,
+  textTheme: const TextTheme(
+    bodySmall: TextStyle(fontSize: 16),
+    bodyLarge: TextStyle(fontSize: 18),
+    bodyMedium: TextStyle(fontSize: 16),
+    headlineMedium: TextStyle(
+      fontWeight: FontWeight.w600,
+    ),
+    titleMedium: TextStyle(
+    color: Colors.grey
+    ),
+  ),
+);
