730b49268ca48c4e5f79c066da3bb49e7f2fd14d
honketh <jzgombic3@gmail.com>
2024-05-28 17:36:46 -0400

    Add more buttons and formatting to drawer
    The buttons still do nothing.

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -9,55 +9,107 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('GPT Thing'),
       ),
-
       drawer: Drawer(
         shape: const LinearBorder(),
-        child: ListView(
-          padding: const EdgeInsets.all(8.0),       
+        child: Column(
           children: [
-            const ListTile(
-              title: Text('GPT Thing'),
-            ),
-            ListTile(
-              title: const Text('ChatGPT'),
-              titleTextStyle: Theme.of(context).textTheme.bodySmall,
-              leading: const Icon(Icons.chat_bubble_rounded),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
+            ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8.0),
+              children: [
+                const ListTile(
+                  title: Text('GPT Thing'),
                 ),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('DALL·E'),
-              titleTextStyle: Theme.of(context).textTheme.bodySmall,
-              leading: const Icon(Icons.design_services_rounded),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
+                ListTile(
+                  title: const Text('ChatGPT'),
+                  titleTextStyle: Theme.of(context).textTheme.bodySmall,
+                  leading: const Icon(Icons.chat_bubble_rounded),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Login'),
-              titleTextStyle: Theme.of(context).textTheme.bodySmall,
-              leading: const Icon(Icons.login_rounded),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
+                ListTile(
+                  title: const Text('DALL·E'),
+                  titleTextStyle: Theme.of(context).textTheme.bodySmall,
+                  leading: const Icon(Icons.design_services_rounded),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
-              tileColor: Colors.green,
-              onTap: () {
-                Navigator.pop(context);
-              },
+              ],
             ),
+            Expanded(
+              child: ListView(
+                reverse: true,
+                padding: const EdgeInsets.all(8.0),
+                children: [
+                  ListTile(
+                    title: const Text('About'),
+                    titleTextStyle: Theme.of(context).textTheme.bodySmall,
+                    leading: const Icon(Icons.info_rounded),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Login'),
+                    titleTextStyle: Theme.of(context).textTheme.bodySmall,
+                    leading: const Icon(Icons.login_rounded),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Sign Up'),
+                    titleTextStyle: Theme.of(context).textTheme.bodySmall,
+                    leading: const Icon(Icons.person_add_rounded),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                    tileColor: Colors.green[600],
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Enter API Key'),
+                    titleTextStyle: Theme.of(context).textTheme.bodySmall,
+                    leading: const Icon(Icons.key_rounded),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                    tileColor: Colors.blue[800],
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
