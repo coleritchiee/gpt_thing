d1b523eeb1b9a137a9c3a3d9d3c48935f438c628
honketh <jzgombic3@gmail.com>
2024-06-09 02:21:45 -0400

    Change messages to library object types
    To help adapt for storing in the database, and future proof it to handle different types of content in messages.

--- lib/home/api_manager.dart
+++ lib/home/api_manager.dart
@@ -1,21 +1,8 @@
 import 'package:dart_openai/dart_openai.dart';
-import 'package:gpt_thing/home/chat_data.dart';
 
 class APIManager {
-  Future<OpenAIChatCompletionModel> chatPrompt(List<ChatMessage> history) async {
-    final List<OpenAIChatCompletionChoiceMessageModel> messages = [];
-
-    for (int i = 0; i < history.length; i++) {
-      messages.add(OpenAIChatCompletionChoiceMessageModel(
-        role: history[i].role,
-        content: [
-          OpenAIChatCompletionChoiceMessageContentItemModel.text(
-            history[i].message,
-          ),
-        ]
-      ));
-    }
-
+  Future<OpenAIChatCompletionModel> chatPrompt(
+    List<OpenAIChatCompletionChoiceMessageModel> messages) async {
     return await OpenAI.instance.chat.create(
       model: "gpt-3.5-turbo",
       messages: messages,
