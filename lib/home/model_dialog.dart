91e86a6772615bbd54d0b173ffdd3becf17bca15
jzgom067 <jzgombic3@gmail.com>
2024-06-21 18:06:07 -0400

    First iteration of new model dialog
    It's nowhere near perfect, but it has the basic idea of separating the groups and models.

--- lib/home/model_dialog.dart
+++ lib/home/model_dialog.dart
@@ -5,13 +5,14 @@ class ModelDialog extends StatelessWidget {
   final ChatData data;
 
   const ModelDialog({super.key, required this.data});
-  
+
   @override
   Widget build(BuildContext context) {
     String filter = data.groups.first.name;
     String newModel = data.model;
     String newGroup = data.modelGroup;
     bool showPreviews = false;
+    bool groupSelected = false;
 
     void setModel() {
       data.setModel(newModel, newGroup);
@@ -21,151 +22,148 @@ class ModelDialog extends StatelessWidget {
     return Dialog(
       insetPadding: const EdgeInsets.all(24.0),
       child: ConstrainedBox(
-        constraints: const BoxConstraints (
-          maxWidth: 600,
+        constraints: const BoxConstraints(
+          maxWidth: 400,
           maxHeight: 500,
         ),
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-          child: StatefulBuilder( // so the right column can update from the left
-            builder: (context, setState) {
-              return Column(
-                children: [
-                  const Text(
-                    "Choose a Model",
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: StatefulBuilder(builder: (context, setState) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  "Choose a Model",
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
-                  Text(
-                    newModel.isEmpty ? "None Selected" : "Selected: $newModel",
-                    style: const TextStyle(
-                      color: Colors.grey,
-                      fontSize: 12,
-                    ),
+                ),
+                Text(
+                  newModel.isEmpty ? "None Selected" : "Selected: $newModel",
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
                   ),
-                  const SizedBox(height: 8),
-                  const Divider(
-                    height: 1,
-                  ),
-                  Expanded(
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: data.groups.map((group) {
-                              return Expanded(
-                                child: ListTile(
-                                  onTap: () {
-                                    setState(() {
-                                      filter = group.name;
-                                    });
-                                  },
-                                  title: Text(group.name),
-                                  subtitle: Text(group.description),
-                                  subtitleTextStyle: const TextStyle(
+                ),
+                const SizedBox(height: 8),
+                const Divider(
+                  height: 1,
+                ),
+                !groupSelected
+                    ? Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: data.groups.map((group) {
+                          return ListTile(
+                            onTap: () {
+                              setState(() {
+                                filter = group.name;
+                                groupSelected = true;
+                              });
+                            },
+                            title: Text(group.name),
+                            subtitle: Text(group.description),
+                            subtitleTextStyle: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                            ),
+                            tileColor:
+                                filter == group.name ? Colors.grey[850] : null,
+                          );
+                        }).toList(),
+                      )
+                    : ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          maxHeight: 300,
+                        ),
+                        child: ListView(
+                          children: data.models.where((i) {
+                            return (i.group == filter) &&
+                                (showPreviews || !i.preview);
+                          }).map((model) {
+                            return ListTile(
+                              onTap: () {
+                                setState(() {
+                                  if (newModel == model.id) {
+                                    setModel(); // detecting "double tap"
+                                  }
+                                  newModel = model.id;
+                                  newGroup = model.group;
+                                });
+                              },
+                              title: Center(child: Text(model.id)),
+                              titleTextStyle: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                              dense: true,
+                              visualDensity: const VisualDensity(vertical: -4),
+                              tileColor: newModel == model.id
+                                  ? Colors.grey[850]
+                                  : null,
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                if (groupSelected)
+                  Column(
+                    children: [
+                      const Divider(height: 1),
+                      const SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 8,
+                          right: 8,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              child: const Text("Cancel"),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                const Text(
+                                  "Preview Models",
+                                  style: TextStyle(
                                     color: Colors.grey,
                                     fontSize: 12,
                                   ),
-                                  tileColor: filter == group.name
-                                    ? Colors.grey[850]
-                                    : null,
                                 ),
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                        const VerticalDivider(
-                          width: 1,
-                        ),
-                        Expanded(
-                          child: ListView(
-                            children: data.models.where((i) {
-                              return (i.group == filter) && (showPreviews || !i.preview);
-                            }
-                            ).map((model) {
-                              return ListTile(
-                                onTap: () {
-                                  setState(() {
-                                    if (newModel == model.id) {
-                                      setModel(); // detecting "double tap"
-                                    }
-                                    newModel = model.id;
-                                    newGroup = model.group;
-                                  });
-                                },
-                                title: Center(child: Text(model.id)),
-                                titleTextStyle: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 14,
+                                Checkbox(
+                                  onChanged: (value) {
+                                    setState(() {
+                                      showPreviews = (value)!;
+                                    });
+                                  },
+                                  value: showPreviews,
+                                  side: BorderSide(
+                                    color: (Colors.grey[500])!,
+                                  ),
+                                  activeColor: Colors.grey[500],
                                 ),
-                                dense: true,
-                                visualDensity: const VisualDensity(vertical: -4),
-                                tileColor: newModel == model.id
-                                  ? Colors.grey[850]
-                                  : null,
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Divider(height: 1),
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 8,
-                      right: 8,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          child: const Text("Cancel"),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            const Text(
-                              "Show Preview Models",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 12,
-                              ),
+                              ],
                             ),
-                            Checkbox(
-                              onChanged: (value) {
-                                setState(() {
-                                  showPreviews = (value)!;
-                                });
-                              },
-                              value: showPreviews,
-                              side: BorderSide(
-                                color: (Colors.grey[500])!,
-                              ),
-                              activeColor: Colors.grey[500],
+                            TextButton(
+                              onPressed: newModel.isEmpty
+                                  ? null
+                                  : () {
+                                      setModel();
+                                    },
+                              child: const Text("Confirm"),
                             ),
                           ],
                         ),
-                        TextButton(
-                          onPressed: newModel.isEmpty ? null : () {
-                            setModel();
-                          },
-                          child: const Text("Confirm"),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              );
-            }
-          ),
+              ],
+            );
+          }),
         ),
       ),
     );
