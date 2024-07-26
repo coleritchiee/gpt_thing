b2a116d5a036917ef8fbd6e404298dcb76e49b96
jzgom067 <jzgombic3@gmail.com>
2024-07-25 22:15:11 -0400

    Add router error handling
    Any route that doesn't exist will just return to home.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:gpt_thing/services/user_locator.dart';
 import 'package:gpt_thing/theme.dart';
 import 'package:gpt_thing/firebase_options.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
@@ -16,7 +15,7 @@ Future<void> main() async {
   ]);
   UserLocator.setupLocator();
   runApp(
-  const App(),
+    const App(),
   );
 }
 
@@ -28,56 +27,68 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final Future<FirebaseApp> _initialization =
+      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final String version = "v1.0.0";
-
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text("${snapshot.error}", textDirection: TextDirection.rtl);
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return FutureBuilder(
-              future: FirestoreService().fetchCurrentVersion(),
-              builder: (context, versionSnapshot) {
-                if (versionSnapshot.connectionState == ConnectionState.waiting) {
-                  return const MaterialApp(home: Scaffold(body: Center(child: CircularProgressIndicator())));
-                } else if (versionSnapshot.hasError) {
-                  return MaterialApp(home: Scaffold(body: Center(child: Text("Error fetching version info: ${versionSnapshot.error}"))));
-                }
-
-                if (versionSnapshot.data != version) {
-                  return MaterialApp(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text("${snapshot.error}", textDirection: TextDirection.rtl);
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return FutureBuilder(
+            future: FirestoreService().fetchCurrentVersion(),
+            builder: (context, versionSnapshot) {
+              if (versionSnapshot.connectionState == ConnectionState.waiting) {
+                return const MaterialApp(
                     home: Scaffold(
-                      body: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text("Update Available", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                            const SizedBox(height: 20),
-                            Text("Your app version is $version, but the latest version is ${versionSnapshot.data}. Please update the app."),
-                            const SizedBox(height: 20),
-                          ],
-                        ),
+                        body: Center(child: CircularProgressIndicator())));
+              } else if (versionSnapshot.hasError) {
+                return MaterialApp(
+                    home: Scaffold(
+                        body: Center(
+                            child: Text(
+                                "Error fetching version info: ${versionSnapshot.error}"))));
+              }
+
+              if (versionSnapshot.data != version) {
+                return MaterialApp(
+                  home: Scaffold(
+                    body: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Update Available",
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 20),
+                          Text(
+                              "Your app version is $version, but the latest version is ${versionSnapshot.data}. Please update the app."),
+                          const SizedBox(height: 20),
+                        ],
                       ),
                     ),
-                  );
-                }
-                return MaterialApp.router(
-                  routerConfig: GoRouter(
-                    routes: appRoutes,
                   ),
-                  theme: appTheme,
                 );
-              },
-            );
-          }
-          return const Text('loading', textDirection: TextDirection.rtl);
-        },
+              }
+              return MaterialApp.router(
+                routerConfig: GoRouter(
+                  onException: (context, state, router) {
+                    router.go('/');
+                  },
+                  routes: appRoutes,
+                ),
+                theme: appTheme,
+              );
+            },
+          );
+        }
+        return const Text('loading', textDirection: TextDirection.rtl);
+      },
     );
   }
 }
