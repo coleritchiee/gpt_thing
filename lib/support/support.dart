ee51fb373d1debeff05c7b9b34af1a5ee74dbce9
jzgom067 <jzgombic3@gmail.com>
2024-08-19 15:09:38 -0400

    Add feedback to email copy
    We should at least do the bare minimum to help out the user here.

--- lib/support/support.dart
+++ lib/support/support.dart
@@ -32,34 +32,43 @@ class SupportPage extends StatelessWidget {
                   style: Theme.of(context).textTheme.titleMedium,
                   textAlign: TextAlign.center,
                 ),
-                StatefulBuilder(builder: (context, setState) {
-                  return MouseRegion(
-                    onEnter: (event) {
-                      setState(() {
-                        textHover = true;
-                      });
-                    },
-                    onExit: (event) {
-                      setState(() {
-                        textHover = false;
-                      });
-                    },
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: () {
-                        Clipboard.setData(const ClipboardData(
-                            text: "silverpangolin.contact@gmail.com"));
+                Tooltip(
+                  message: "Copy email",
+                  child: StatefulBuilder(builder: (context, setState) {
+                    return MouseRegion(
+                      onEnter: (event) {
+                        setState(() {
+                          textHover = true;
+                        });
                       },
-                      child: Text("silverpangolin.contact@gmail.com",
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary,
-                            decoration:
-                                textHover ? TextDecoration.underline : null,
-                            decorationColor: Theme.of(context).colorScheme.primary,
-                          )),
-                    ),
-                  );
-                }),
+                      onExit: (event) {
+                        setState(() {
+                          textHover = false;
+                        });
+                      },
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () {
+                          Clipboard.setData(const ClipboardData(
+                              text: "silverpangolin.contact@gmail.com"));
+                          ScaffoldMessenger.of(context).clearSnackBars();
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content: Text("Email copied to clipboard"),
+                          ));
+                        },
+                        child: Text("silverpangolin.contact@gmail.com",
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              decoration:
+                                  textHover ? TextDecoration.underline : null,
+                              decorationColor:
+                                  Theme.of(context).colorScheme.primary,
+                            )),
+                      ),
+                    );
+                  }),
+                ),
                 const SizedBox(height: 20),
                 Center(
                   child: ElevatedButton(
